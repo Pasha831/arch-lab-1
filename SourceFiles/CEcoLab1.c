d09d15414d6099a050a8c197cddd80d1ce7ce389
Pasha Medvedev <pashamedvedev03@gmail.com>
2024-02-28 00:40:35 +0300

    first commit

---
+++ SourceFiles/CEcoLab1.c
@@ -0,0 +1,360 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   CEcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный код описывает реализацию интерфейсов CEcoLab1
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#include "IEcoSystem1.h"
+#include "IEcoInterfaceBus1.h"
+#include "IEcoInterfaceBus1MemExt.h"
+#include "CEcoLab1.h"
+
+/*
+ *
+ * <сводка>
+ *   Функция QueryInterface
+ * </сводка>
+ *
+ * <описание>
+ *   Функция QueryInterface для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD CEcoLab1_QueryInterface(/* in */ struct IEcoLab1* me, /* in */ const UGUID* riid, /* out */ void** ppv) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателей */
+    if (me == 0 || ppv == 0) {
+        return -1;
+    }
+
+    /* Проверка и получение запрошенного интерфейса */
+    if ( IsEqualUGUID(riid, &IID_IEcoLab1) ) {
+        *ppv = &pCMe->m_pVTblIEcoLab1;
+        pCMe->m_pVTblIEcoLab1->AddRef((IEcoLab1*)pCMe);
+    }
+    else if ( IsEqualUGUID(riid, &IID_IEcoUnknown) ) {
+        *ppv = &pCMe->m_pVTblIEcoLab1;
+        pCMe->m_pVTblIEcoLab1->AddRef((IEcoLab1*)pCMe);
+    }
+    else {
+        *ppv = 0;
+        return -1;
+    }
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция AddRef
+ * </сводка>
+ *
+ * <описание>
+ *   Функция AddRef для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+uint32_t ECOCALLMETHOD CEcoLab1_AddRef(/* in */ struct IEcoLab1* me) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1;
+    }
+
+    return ++pCMe->m_cRef;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Release
+ * </сводка>
+ *
+ * <описание>
+ *   Функция Release для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+uint32_t ECOCALLMETHOD CEcoLab1_Release(/* in */ struct IEcoLab1* me) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1;
+    }
+
+    /* Уменьшение счетчика ссылок на компонент */
+    --pCMe->m_cRef;
+
+    /* В случае обнуления счетчика, освобождение данных экземпляра */
+    if ( pCMe->m_cRef == 0 ) {
+        deleteCEcoLab1((IEcoLab1*)pCMe);
+        return 0;
+    }
+    return pCMe->m_cRef;
+}
+
+// Функция для обмена значений между двумя элементами
+void swap(struct IEcoLab1* me, char* first, char* second, size_t size) {
+	CEcoLab1* pCMe = (CEcoLab1*)me;
+	char* buffer = (char*)pCMe->m_pIMem->pVTbl->Alloc(pCMe->m_pIMem, size);
+
+	pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, buffer, first, size);
+	pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, first, second, size);
+	pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, second, buffer, size);
+	pCMe->m_pIMem->pVTbl->Free(pCMe->m_pIMem, buffer);
+}
+
+// Функция для превращения поддерева с корнем i в пирамиду
+void heapify(
+	struct IEcoLab1* me,
+	char* buffer,
+	size_t elem_count,
+	size_t elem_size,
+	int (__cdecl *comp)(const void *, const void*),
+	int i
+) {
+    int largest = i; // Инициализируем наибольший элемент как корень
+    int left = 2 * i + 1; // Инициализируем левый дочерний элемент
+    int right = 2 * i + 2; // Инициализируем правый дочерний элемент
+
+    // Если левый дочерний элемент больше корня
+    if (left < elem_count && comp(buffer + left * elem_size, buffer + largest * elem_size) > 0) {
+		largest = left;
+	}
+
+    // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+    if (right < elem_count && comp(buffer + right * elem_size, buffer + largest * elem_size) > 0) {
+        largest = right;
+	}
+
+    // Если самый большой элемент не корень
+    if (largest != i) {
+        swap(me, buffer + i * elem_size, buffer + largest * elem_size, elem_size);
+        // Продолжаем рекурсивно heapify для поддерева, которое изменилось
+        heapify(me, buffer, elem_count, elem_size, comp, largest);
+    }
+}
+
+// Основная функция пирамидальной сортировки
+void heapsort(
+	struct IEcoLab1* me,
+	size_t elem_count,
+	size_t elem_size,
+	int (__cdecl *comp)(const void *, const void*),
+	char* buffer
+) {
+	int count;
+
+    // Построение пирамиды (перегруппируем массив)
+    for (count = elem_count / 2 - 1; count >= 0; count--) {
+		heapify(me, buffer, elem_count, elem_size, comp, count);
+	}
+
+    // Извлекаем элементы из пирамиды по одному
+    for (count = elem_count - 1; count >= 0; count--) {
+        // Перемещаем текущий корень в конец
+        swap(me, buffer, buffer + count * elem_size, elem_size);
+        // Вызываем heapify на уменьшенной пирамиде
+        heapify(me, buffer, count, elem_size, comp, 0);
+    }
+}
+
+// Функция пирамидальной сортировки
+int16_t ECOCALLMETHOD CEcoLab1_heapsort(
+	struct IEcoLab1* me, 
+	void* pData,
+	size_t elem_count,
+	size_t elem_size,
+	int (__cdecl *comp)(const void *, const void *)
+) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    size_t index = 0;
+	void *buffer = 0;
+
+    /* Проверка указателей */
+    if (me == 0 || pData == 0 || comp == 0) {
+        return -1;
+    }
+
+	// Выделение памяти под буффер
+	buffer = pCMe->m_pIMem->pVTbl->Alloc(pCMe->m_pIMem, elem_count * elem_size);
+	pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, buffer, pData, elem_count * elem_size);
+
+	// Сортировка буффера
+	heapsort(me, elem_count, elem_size, comp, (char *) buffer);
+	pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, pData, buffer, elem_count * elem_size);
+
+    // Очистка буффера
+	pCMe->m_pIMem->pVTbl->Free(pCMe->m_pIMem, buffer);
+
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Init
+ * </сводка>
+ *
+ * <описание>
+ *   Функция инициализации экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD initCEcoLab1(/*in*/ struct IEcoLab1* me, /* in */ struct IEcoUnknown *pIUnkSystem) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    IEcoInterfaceBus1* pIBus = 0;
+    int16_t result = -1;
+
+    /* Проверка указателей */
+    if (me == 0) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pCMe->m_pISys->pVTbl->QueryInterface(pCMe->m_pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+    /* Проверка указателей */
+    if (me == 0 ) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+	
+    return result;
+}
+
+/* Create Virtual Table IEcoLab1 */
+IEcoLab1VTbl g_x277FC00C35624096AFCFC125B94EEC90VTbl = {
+    CEcoLab1_QueryInterface,
+    CEcoLab1_AddRef,
+    CEcoLab1_Release,
+    CEcoLab1_heapsort
+};
+
+
+/*
+ *
+ * <сводка>
+ *   Функция Create
+ * </сводка>
+ *
+ * <описание>
+ *   Функция создания экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD createCEcoLab1(/* in */ IEcoUnknown* pIUnkSystem, /* in */ IEcoUnknown* pIUnkOuter, /* out */ IEcoLab1** ppIEcoLab1) {
+    int16_t result = -1;
+    IEcoSystem1* pISys = 0;
+    IEcoInterfaceBus1* pIBus = 0;
+    IEcoInterfaceBus1MemExt* pIMemExt = 0;
+    IEcoMemoryAllocator1* pIMem = 0;
+    CEcoLab1* pCMe = 0;
+    UGUID* rcid = (UGUID*)&CID_EcoMemoryManager1;
+	
+    /* Проверка указателей */
+    if (ppIEcoLab1 == 0 || pIUnkSystem == 0) {
+        return result;
+    }
+
+    /* Получение системного интерфейса приложения */
+    result = pIUnkSystem->pVTbl->QueryInterface(pIUnkSystem, &GID_IEcoSystem1, (void **)&pISys);
+
+    /* Проверка */
+    if (result != 0 && pISys == 0) {
+        return result;
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+	/* Получение идентификатора компонента для работы с памятью */
+    result = pIBus->pVTbl->QueryInterface(pIBus, &IID_IEcoInterfaceBus1MemExt, (void**)&pIMemExt);
+    if (result == 0 && pIMemExt != 0) {
+        rcid = (UGUID*)pIMemExt->pVTbl->get_Manager(pIMemExt);
+        pIMemExt->pVTbl->Release(pIMemExt);
+    }
+
+    /* Получение интерфейса распределителя памяти */
+    pIBus->pVTbl->QueryComponent(pIBus, rcid, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 && pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        pISys->pVTbl->Release(pISys);
+        return result;
+    }
+
+    /* Выделение памяти для данных экземпляра */
+    pCMe = (CEcoLab1*)pIMem->pVTbl->Alloc(pIMem, sizeof(CEcoLab1));
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = pISys;
+
+    /* Сохранение указателя на интерфейс для работы с памятью */
+    pCMe->m_pIMem = pIMem;
+
+    /* Установка счетчика ссылок на компонент */
+    pCMe->m_cRef = 1;
+
+    /* Создание таблицы функций интерфейса IEcoLab1 */
+    pCMe->m_pVTblIEcoLab1 = &g_x277FC00C35624096AFCFC125B94EEC90VTbl;
+
+    /* Возврат указателя на интерфейс */
+    *ppIEcoLab1 = (IEcoLab1*)pCMe;
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Delete
+ * </сводка>
+ *
+ * <описание>
+ *   Функция освобождения экземпляра
+ * </описание>
+ *
+ */
+void ECOCALLMETHOD deleteCEcoLab1(/* in */ IEcoLab1* pIEcoLab1) {
+    CEcoLab1* pCMe = (CEcoLab1*)pIEcoLab1;
+    IEcoMemoryAllocator1* pIMem = 0;
+
+    if (pIEcoLab1 != 0 ) {
+        pIMem = pCMe->m_pIMem;
+        /* Освобождение */
+        if ( pCMe->m_pISys != 0 ) {
+            pCMe->m_pISys->pVTbl->Release(pCMe->m_pISys);
+        }
+        pIMem->pVTbl->Free(pIMem, pCMe);
+        pIMem->pVTbl->Release(pIMem);
+    }
+}
